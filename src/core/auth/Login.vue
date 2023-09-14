2697ca5dca4e51a002e06cb71cb65849979dbb2c
jorge9815 <jorge9815rdz@gmail.com>
2023-09-14 10:55:06 -0400

    first commit

---
+++ src/core/auth/Login.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full h-full">
+    <div class="bg-blue-800 flex content-center justify-center h-full">
+      <div class="flex-col content-start w-4/5">
+        <div class="businessTitle">
+          <h1>Bienvenido a</h1>
+          <h1><span>Little</span> Business</h1>
+        </div>
+        <div class="googleAuth">
+          <div></div>
+          <div></div>
+        </div>
+        <p>ó</p>
+        <div>
+          <form action="">
+            <div>
+              <label for="user">Usuario</label><br />
+              <input type="text" id="user" name="fname" value="" /><br />
+            </div>
+            <div>
+              <label for="password">Contraseña</label><br />
+              <input type="text" id="password" name="lname" value="" /><br /><br />
+            </div>
+            <input type="submit" value="Entrar" />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* businessTitle{
+
+    } */
+</style>
